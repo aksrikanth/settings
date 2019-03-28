e026846ac9ced8a9f9d9a843a6af1f152b2304bb
Srikanth Agaram <ak@srikanthak.name>
2019-03-28 15:21:55 -0700

    Update whiteboard cleaner to quantize colors to produce smaller output

--- bin/whiteboard_clean.sh
+++ bin/whiteboard_clean.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "$2"
+# Needs ImageMagick.
+# original GIMP algorithm: matthew.mceachen.us/blog/how-to-clean-up-photos-of-whiteboards-with-gimp-403.html
+# imagemagick solution: https://gist.github.com/lelandbatey/8677901
+# original command: convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "$2"
+convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 +dither -posterize 3 "$2"
